903d296f12fc394f921fb2cb9af2e569ab38bf64
CTTPamela <pamela.chang@philips.com>
2025-07-18 10:33:08 +0800

    Assignment done on 18 July 2025

---
+++ tests.py
@@ -0,0 +1,21 @@
+# tests.py
+from color_code_converter import get_color_from_pair_number, get_pair_number_from_color
+from reference_manual import generate_reference_manual
+from constants import MAJOR_COLORS, MINOR_COLORS
+
+def test_number_to_pair(pair_number,
+                        expected_major_color, expected_minor_color):
+  major_color, minor_color = get_color_from_pair_number(pair_number)
+  assert(major_color == expected_major_color)
+  assert(minor_color == expected_minor_color)
+ 
+ 
+def test_pair_to_number(major_color, minor_color, expected_pair_number):
+  pair_number = get_pair_number_from_color(major_color, minor_color)
+  assert(pair_number == expected_pair_number)
+
+def test_reference_manual_length():
+    manual = generate_reference_manual()
+    lines = manual.split('\n')
+    # 5行标题 + 颜色组合数 (5*5=25) + 最后的空行
+    #assert len(lines) == 5 + len(MAJOR_COLORS)*len(MINOR_COLORS) + 1
