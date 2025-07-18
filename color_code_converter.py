da6a098e451a8e7c7ab7799a2462ba8da2292d74
CTTPamela <pamela.chang@philips.com>
2025-07-18 13:55:09 +0800

    Update color_code_converter.py
    Updated get_color_from_pair_number(pair_number)

--- color_code_converter.py
+++ color_code_converter.py
@@ -7,11 +7,13 @@ def color_pair_to_string(major_color, minor_color):
  
 def get_color_from_pair_number(pair_number):
   zero_based_pair_number = pair_number - 1
-  major_index = zero_based_pair_number // len(MINOR_COLORS)
-  if major_index >= len(MAJOR_COLORS):
+  length_major = len(MAJOR_COLORS)
+  length_minor = len(MINOR_COLORS)
+  major_index = zero_based_pair_number // length_major
+  if major_index >= length_major:
     raise Exception('Major index out of range')
-  minor_index = zero_based_pair_number % len(MINOR_COLORS)
-  if minor_index >= len(MINOR_COLORS):
+  minor_index = zero_based_pair_number % length_minor
+  if minor_index >= length_minor:
     raise Exception('Minor index out of range')
   return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
  
